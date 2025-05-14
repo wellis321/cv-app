657dbafb8eed10eac8c6e88d0d736ad194cafd15
wellis321 <wellis321@msn.com>
2025-05-14 17:13:02 +0100

    updated the node runtime

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter({
-            // Use Node.js 18 runtime
-            runtime: 'nodejs18.x',
+            // Update from nodejs18.x to nodejs18
+            runtime: 'nodejs18',
             // Enable regional edge functions
             regions: ['iad1']
         })
