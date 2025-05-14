c14f7006ef1fde54b194dd2da88ee9bb50eee01e
wellis321 <wellis321@msn.com>
2025-05-14 17:19:37 +0100

    updated the adapter

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
         adapter: adapter({
-            // Update from nodejs18.x to nodejs18
-            runtime: 'nodejs18',
+            // Use the correct runtime format for Vercel
+            runtime: 'nodejs18.x',
             // Enable regional edge functions
             regions: ['iad1']
         })
