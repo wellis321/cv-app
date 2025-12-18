c20b3ae7d65f55655be05b50a32f9380a59ed513
wellis321 <wellis321@msn.com>
2025-12-18 21:47:51 +0000

    changed the footer background

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,4 +1,4 @@
-<footer class="bg-white border-t border-gray-200 mt-12">
+<footer class="bg-gray-50 border-t border-gray-200 mt-12">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div class="grid grid-cols-1 md:grid-cols-3 gap-8">
             <div>
